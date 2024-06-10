0bf5307ab77be4314d8f188a88b357e4d1732993
chu-seiken <vuln.chuseiken@gmail.com>
2024-06-10 18:21:18 +0900

    Update navi.js

--- js/navi.js
+++ js/navi.js
@@ -5,7 +5,7 @@ var s = `
 <ul id="topnav">
 	<li class="index1"><a href="index.html">トップページ<br><span>Top</span></a></li>
 	<li class="index2"><a href="about_cvl.html">CVLについて<br><span>About CVL</span></a></li>
-	<li class="index3"><a href="cvl.html">CVL（Chuseiken <br>Vulnability List）</a></li>
+	<li class="index3"><a href="cvl.html">CVL（Chuseiken <br>Vulnerability List）</a></li>
 	<li class="index4"><a href="report.html">定期レポート<br><span>Report</span></a></li>
 	<li class=""><a href="http://chuseiken.or.jp/index.html" target="_blank">当法人について<br><span>About</span></a></li>
 	<li class="index5"><a href="contact.html" target="_blank">脆弱性・情報漏洩の報告<br><span>Contact</span></a></li>
